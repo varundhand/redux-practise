f27c5cf2b8d239de319937efecfbde81cb01261e
varundhand <varundhand77@gmail.com>
2023-04-19 17:30:19 +0530

    created AddPostsForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import AddPostsForm from './components/AddPostsForm'
 import PostsList from './components/PostsList'
 
 
@@ -6,6 +7,7 @@ function App() {
 
   return (
     <main className="App">
+      <AddPostsForm/>
       <PostsList/>
     </main>
   )
--- src/components/AddPostsForm.jsx
+++ src/components/AddPostsForm.jsx
@@ -1,11 +1,58 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";  // generates a random id like uuid
 import React from 'react'
 
+import { postAdded } from "../features/posts/postsSlice";
+
 const AddPostsForm = () => {
+  const dispatch = useDispatch()
+  const [title,setTitle] = useState('')
+  const [content,setContent] = useState('')
+
+  const onTitleChanged = (e) => {setTitle(e.target.value)}
+  const onContentChanged = (e) => {setContent(e.target.value)}
+
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(
+        postAdded({
+          id:nanoid(), // it gives id, title and content
+          title,
+          content
+        })
+      )
+      setTitle('') // setTitle and setContent back to empty string once post is saved
+      setContent('')
+    }
+  }
+
   return (
-    <div>
-      
-    </div>
+    <section>
+      <h2>Add a New Post</h2>
+      <form action="">
+        <label htmlFor="postTitle">Post Title: </label>
+        <input 
+          type="text" 
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postContent">Content: </label>
+        <textarea 
+          id="postContent" 
+          cols="30" rows="10"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button 
+          type="button"
+          onClick={onSavePostClicked}
+        >Post Save</button>
+      </form>
+    </section>
   )
 }
 
